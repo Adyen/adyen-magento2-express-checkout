19467dcbcb6577ed7ba3f8174661e2275a118098
raoulritter <59527829+raoulritter@users.noreply.github.com>
2023-10-23 10:35:11 +0200

    [ECP-8595] - Correctly update tax amount on the GooglePay component when Shipping Address is changed. (#45)
    * [ECP-8595] - Correctly update tax amount on the GooglePay component when Shipping Address is changed.
    
    * [ECP-8595] - Remove the unused values and console.log

--- view/frontend/web/js/helpers/getRegionId.js
+++ view/frontend/web/js/helpers/getRegionId.js
@@ -1,13 +1,14 @@
 define(['Adyen_ExpressCheckout/js/model/countries'], function (countriesModel) {
     'use strict';
 
-    return function (countryCode, regionName) {
-        const countries = countriesModel().getCountires();
-
+    return function (countryCode, regionName, byRegionCode = false) {
+        const countries = countriesModel().getCountires(byRegionCode);
         if (typeof regionName !== 'string') {
             return null;
         }
-        regionName = regionName.toLowerCase().replace(/[^A-Z0-9]/ig, '');
+        if(!byRegionCode){
+            regionName = regionName.toLowerCase().replace(/[^A-Z0-9]/ig, '');
+        }
         if (typeof countries[countryCode] !== 'undefined'
             && typeof countries[countryCode][regionName] !== 'undefined') {
             return countries[countryCode][regionName];
