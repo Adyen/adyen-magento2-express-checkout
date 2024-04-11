2d1a732d13d1c9d1fc09463af282a6a56fcf66b8
raoulritter <59527829+raoulritter@users.noreply.github.com>
2024-04-11 14:29:01 +0200

    [ECP-9082] Implement Checkout API's /paypal/updateOrder endpoint (#86)
    * Implement Checkout API's /paypal/updateOrder endpoint
    
    * [ECP-9082] Obtain order related information from backend and implement guest endpoint
    
    ---------
    
    Co-authored-by: Can Demiralp <can.demiralp@adyen.com>
    Co-authored-by: Can Demiralp <ecandemiralp@gmail.com>

---
+++ Model/AdyenPaypalUpdateOrder.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ *
+ * Adyen ExpressCheckout Module
+ *
+ * Copyright (c) 2024 Adyen N.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+declare(strict_types=1);
+
+namespace Adyen\ExpressCheckout\Model;
+
+use Adyen\AdyenException;
+use Adyen\Client;
+use Adyen\ExpressCheckout\Api\AdyenPaypalUpdateOrderInterface;
+use Adyen\ExpressCheckout\Helper\PaypalUpdateOrder;
+use Adyen\ExpressCheckout\Helper\Util\PaypalDeliveryMethodValidator;
+use Adyen\ExpressCheckout\Model\ResourceModel\PaymentResponse\Collection as AdyenPaymentResponseCollection;
+use Adyen\Payment\Helper\ChargedCurrency;
+use Adyen\Payment\Helper\Data;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\ValidatorException;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Quote\Model\Quote;
+
+class AdyenPaypalUpdateOrder implements AdyenPaypalUpdateOrderInterface
+{
+    /**
+     * @var PaypalUpdateOrder
+     */
+    protected PaypalUpdateOrder $paypalUpdateOrderHelper;
+
+    /**
+     * @var CartRepositoryInterface
+     */
+    private CartRepositoryInterface $cartRepository;
+
+    /**
+     * @var PaypalDeliveryMethodValidator
+     */
+    private PaypalDeliveryMethodValidator $deliveryMethodValidator;
+
+    /**
+     * @var ChargedCurrency
+     */
+    private ChargedCurrency $chargedCurrency;
+
+    /**
+     * @var Data
+     */
+    private Data $adyenHelper;
+
+    /**
+     * @var AdyenPaymentResponseCollection
+     */
+    private AdyenPaymentResponseCollection $paymentResponseCollection;
+
+    /**
+     * @param PaypalUpdateOrder $updatePaypalOrderHelper
+     * @param CartRepositoryInterface $cartRepository
+     * @param PaypalDeliveryMethodValidator $deliveryMethodValidator
+     * @param ChargedCurrency $chargedCurrency
+     * @param Data $adyenHelper
+     * @param AdyenPaymentResponseCollection $paymentResponseCollection
+     */
+    public function __construct(
+        PaypalUpdateOrder $updatePaypalOrderHelper,
+        CartRepositoryInterface $cartRepository,
+        PaypalDeliveryMethodValidator $deliveryMethodValidator,
+        ChargedCurrency $chargedCurrency,
+        Data $adyenHelper,
+        AdyenPaymentResponseCollection $paymentResponseCollection
+    ) {
+        $this->paypalUpdateOrderHelper = $updatePaypalOrderHelper;
+        $this->cartRepository = $cartRepository;
+        $this->deliveryMethodValidator = $deliveryMethodValidator;
+        $this->chargedCurrency = $chargedCurrency;
+        $this->adyenHelper = $adyenHelper;
+        $this->paymentResponseCollection = $paymentResponseCollection;
+    }
+
+    /**
+     * @param int $adyenCartId
+     * @param string $paymentData
+     * @param array $deliveryMethods
+     * @return string
+     * @throws NoSuchEntityException
+     * @throws ValidatorException
+     */
+    public function execute(int $adyenCartId, string $paymentData, $deliveryMethods = []): string
+    {
+        /** @var Quote $quote */
+        $quote = $this->cartRepository->get($adyenCartId);
+        $merchantReference = $quote->getReservedOrderId();
+
+        if (is_null($merchantReference)) {
+            throw new ValidatorException(
+                __('Order ID has not been reserved!')
+            );
+        }
+
+        $paymentResponse = $this->paymentResponseCollection->getPaymentResponseWithMerchantReference(
+            $merchantReference
+        );
+
+        if (is_null($paymentResponse)) {
+            throw new ValidatorException(
+                __('Payment response couldn\'t be found!')
+            );
+        }
+
+        $decodedPaymentResponse = json_decode($paymentResponse['response'], true);
+        if (!isset($decodedPaymentResponse['pspReference'])) {
+            throw new ValidatorException(
+                __('Payment pspreference does not exist in the payment response!')
+            );
+        } else {
+            $pspReference = $decodedPaymentResponse['pspReference'];
+        }
+
+        $storeId = $quote->getStoreId();
+        $deliveryMethods = $this->deliveryMethodValidator->getValidatedDeliveryMethods($deliveryMethods);
+
+        $quoteAmountCurrency = $this->chargedCurrency->getQuoteAmountCurrency($quote);
+        $amountCurrency = $quoteAmountCurrency->getCurrencyCode();
+        $amountValue = $quoteAmountCurrency->getAmount();
+
+        try {
+            $paypalUpdateOrderService = $this->paypalUpdateOrderHelper->createAdyenUtilityApiService($storeId);
+            $paypalUpdateOrderRequest = $this->paypalUpdateOrderHelper->buildPaypalUpdateOrderRequest(
+                $pspReference,
+                $paymentData,
+                $this->adyenHelper->formatAmount($amountValue, $amountCurrency),
+                $amountCurrency,
+                $deliveryMethods
+            );
+
+            $this->adyenHelper->logRequest(
+                $paypalUpdateOrderRequest->toArray(),
+                Client::API_CHECKOUT_VERSION,
+                '/paypal/updateOrder'
+            );
+
+            $paypalUpdateOrderResponse = $paypalUpdateOrderService->updatesOrderForPaypalExpressCheckout(
+                $paypalUpdateOrderRequest
+            );
+        } catch (AdyenException $e) {
+            $errorResponse['error'] = $e->getMessage();
+            $errorResponse['errorCode'] = $e->getAdyenErrorCode();
+
+            $this->adyenHelper->logResponse($errorResponse);
+
+            throw new ValidatorException(
+                __('Error with payment method, please select a different payment method.')
+            );
+        }
+
+        $this->adyenHelper->logResponse($paypalUpdateOrderResponse->toArray());
+
+        return json_encode(
+            $this->paypalUpdateOrderHelper->handlePaypalUpdateOrderResponse($paypalUpdateOrderResponse)
+        );
+    }
+}
---
+++ Model/GuestAdyenPaypalUpdateOrder.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ * Adyen ExpressCheckout Module
+ *
+ * Copyright (c) 2024 Adyen N.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+declare(strict_types=1);
+
+namespace Adyen\ExpressCheckout\Model;
+
+use Adyen\ExpressCheckout\Api\AdyenPaypalUpdateOrderInterface;
+use Adyen\ExpressCheckout\Api\GuestAdyenPaypalUpdateOrderInterface;
+use Magento\Quote\Model\QuoteIdMask;
+use Magento\Quote\Model\QuoteIdMaskFactory;
+
+class GuestAdyenPaypalUpdateOrder implements GuestAdyenPaypalUpdateOrderInterface
+{
+    /**
+     * @var QuoteIdMaskFactory
+     */
+    private QuoteIdMaskFactory $quoteIdMaskFactory;
+
+    /**
+     * @var AdyenPaypalUpdateOrder
+     */
+    private AdyenPaypalUpdateOrder $adyenUpdatePaypalOrder;
+
+    /**
+     * @param QuoteIdMaskFactory $quoteIdMaskFactory
+     * @param AdyenPaypalUpdateOrderInterface $adyenUpdatePaypalOrder
+     */
+    public function __construct(
+        QuoteIdMaskFactory $quoteIdMaskFactory,
+        AdyenPaypalUpdateOrderInterface $adyenUpdatePaypalOrder
+    ) {
+        $this->quoteIdMaskFactory = $quoteIdMaskFactory;
+        $this->adyenUpdatePaypalOrder = $adyenUpdatePaypalOrder;
+    }
+
+    public function execute(string $maskedQuoteId, string $paymentData, $deliveryMethods = []): string
+    {
+        /** @var $quoteIdMask QuoteIdMask */
+        $quoteIdMask = $this->quoteIdMaskFactory->create()->load(
+            $maskedQuoteId,
+            'masked_id'
+        );
+        $quoteId = (int) $quoteIdMask->getQuoteId();
+
+        return $this->adyenUpdatePaypalOrder->execute($quoteId, $paymentData, $deliveryMethods);
+    }
+}
