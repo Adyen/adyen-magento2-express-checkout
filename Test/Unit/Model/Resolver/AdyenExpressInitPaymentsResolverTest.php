7b55e2eb4980b1babe63224bb2237755d1b80720
Can Demiralp <can.demiralp@adyen.com>
2024-10-29 11:04:48 +0100

    [ECP-9496] Fix typo

--- Test/Unit/Model/Resolver/AdyenExpressInitPaymentsResolverTest.php
+++ Test/Unit/Model/Resolver/AdyenExpressInitPaymentsResolverTest.php
@@ -55,7 +55,7 @@ class AdyenExpressInitPaymentsResolverTest extends AbstractAdyenResolverTestCase
         return [
             [
                 'args' => [
-                    'adyenCartId', 'adyenMaskedQuoteId'
+                    'adyenCartId' => 'adyenMaskedQuoteId'
                 ]
             ],
             [
