ae9d936aea6c9e705613dc971363401721f30e51
Can Demiralp <ecandemiralp@gmail.com>
2024-11-08 09:27:59 +0100

    [ECP-9496] Implement GraphQL mutations required for PayPal express payments (#136)
    * [ECP-9496] Implement initPayments GraphQL mutation
    
    * [ECP-9496] Rename the mutation to remove ambiguity
    
    * [ECP-9496] Implement adyenExpressPaypalUpdateOrder GraphQL mutation
    
    * [ECP-9496] Create abstract resolver test case and write tests
    
    * [ECP-9496] Update unit tests
    
    * [ECP-9496] Refactor method return callback
    
    * Revert "[ECP-9496] Refactor method return callback"
    
    This reverts commit 6918816e526d8e86cded2fb4ab811f643a8388b7.
    
    * [ECP-9496] Formatting
    
    * [ECP-9496] Fix typo
    
    * [ECP-9496] Write unit test
    
    * [ECP-9496] Use abstract resolver test class for all resolver unit tests
    
    * [ECP-9496] Write GraphQL API functional tests for new mutations
    
    ---------
    
    Co-authored-by: Can Demiralp <can.demiralp@adyen.com>

--- Test/Unit/Model/Resolver/ExpressInitResolverTest.php
+++ Test/Unit/Model/Resolver/ExpressInitResolverTest.php
@@ -12,58 +12,39 @@
 namespace Adyen\ExpressCheckout\Test\Unit\Model\Resolver;
 
 use Adyen\ExpressCheckout\Api\Data\ProductCartParamsInterfaceFactory;
-use Adyen\ExpressCheckout\Exception\ExpressInitException;
 use Adyen\ExpressCheckout\Model\ExpressInit;
 use Adyen\ExpressCheckout\Model\ProductCartParams;
 use Adyen\ExpressCheckout\Model\Resolver\ExpressInitResolver;
-use Adyen\Payment\Logger\AdyenLogger;
-use Adyen\Payment\Test\Unit\AbstractAdyenTestCase;
-use Laminas\Soap\Client\Local;
+use Exception;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
-use Magento\Framework\GraphQl\Query\Resolver\Value;
-use Magento\Framework\GraphQl\Query\Resolver\ValueFactory;
-use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Magento\GraphQl\Model\Query\Context;
-use Magento\Quote\Model\QuoteIdMask;
-use Magento\Quote\Model\QuoteIdMaskFactory;
 use PHPUnit\Framework\MockObject\MockObject;
 
-class ExpressInitResolverTest extends AbstractAdyenTestCase
+class ExpressInitResolverTest extends AbstractAdyenResolverTestCase
 {
-    // Mocks for class builder
-    protected ?ExpressInitResolver $expressInitResolver;
-    protected MockObject&ExpressInit $expressInitMock;
-    protected MockObject&ProductCartParamsInterfaceFactory $cartParamsInterfaceFactoryMock;
-    protected MockObject&ValueFactory $valueFactoryMock;
-    protected MockObject&QuoteIdMaskFactory $quoteIdMaskFactoryMock;
-    protected MockObject&AdyenLogger $loggerMock;
-
-    // Mocks for `resolve()` method
-    protected MockObject&Field $fieldMock;
-    protected MockObject&Context $contextMock;
-    protected MockObject&ResolveInfo $infoMock;
-
     const SUCCESSFUL_CART_PARAMS = "{\"product\":1562,\"qty\":\"5\",\"super_attribute\":{\"93\":56,\"144\":166}}";
 
+    protected MockObject&ExpressInit $expressInitMock;
+    protected MockObject&ProductCartParamsInterfaceFactory $cartParamsInterfaceFactoryMock;
+
+    /**
+     * Build the class under test and the dependencies
+     *
+     * @return void
+     */
     public function setUp(): void
     {
+        // Build generic mocks for Adyen resolver tests
+        parent::setUp();
+
+        // Build class specific mocks
         $this->expressInitMock = $this->createMock(ExpressInit::class);
         $this->cartParamsInterfaceFactoryMock = $this->createGeneratedMock(
             ProductCartParamsInterfaceFactory::class,
             ['create']
         );
-        $this->valueFactoryMock = $this->createMock(ValueFactory::class);
-        $this->fieldMock = $this->createMock(Field::class);
-        $this->contextMock = $this->createMock(Context::class);
-        $this->infoMock = $this->createMock(ResolveInfo::class);
-        $this->quoteIdMaskFactoryMock = $this->createGeneratedMock(QuoteIdMaskFactory::class, [
-            'create'
-        ]);
-        $this->loggerMock = $this->createMock(AdyenLogger::class);
 
-        $this->expressInitResolver = new ExpressInitResolver(
+        $this->resolver = new ExpressInitResolver(
             $this->expressInitMock,
             $this->cartParamsInterfaceFactoryMock,
             $this->valueFactoryMock,
@@ -73,13 +54,62 @@ class ExpressInitResolverTest extends AbstractAdyenTestCase
     }
 
     /**
-     * Reset the target class after each test
+     * See the data provider for test case explanations.
+     *
+     * @dataProvider invalidProductCartParamsDataProvider
      *
      * @return void
+     * @throws GraphQlInputException|LocalizedException|Exception
      */
-    public function tearDown(): void
+    public function testProductCartParamsShouldThrowException($productCartParams)
     {
-        $this->expressInitResolver = null;
+        $this->expectException(GraphQlInputException::class);
+
+        $args = [
+            'productCartParams' => $productCartParams
+        ];
+
+        $this->resolver->resolve(
+            $this->fieldMock,
+            $this->contextMock,
+            $this->infoMock,
+            [],
+            $args
+        );
+    }
+
+    /**
+     * This test case extends the abstract testSuccessfulResolving() test case
+     *
+     * @dataProvider successfulResolverDataProvider
+     *
+     * @param $args
+     * @return void
+     * @throws Exception
+     */
+    public function testSuccessfulResolving($args)
+    {
+        $productCartParamsMock = $this->createMock(ProductCartParams::class);
+        $this->cartParamsInterfaceFactoryMock->method('create')->willReturn($productCartParamsMock);
+
+        parent::testSuccessfulResolving($args);
+    }
+
+    /**
+     * This test case extends the abstract testMissingQuoteShouldThrowException() test case
+     *
+     * @dataProvider missingQuoteAssertionDataProvider
+     *
+     * @param $args
+     * @return void
+     * @throws Exception
+     */
+    public function testMissingQuoteShouldThrowException($args)
+    {
+        $productCartParamsMock = $this->createMock(ProductCartParams::class);
+        $this->cartParamsInterfaceFactoryMock->method('create')->willReturn($productCartParamsMock);
+
+        parent::testSuccessfulResolving($args);
     }
 
     /**
@@ -87,7 +117,7 @@ class ExpressInitResolverTest extends AbstractAdyenTestCase
      *
      * @return array[]
      */
-    public static function invalidProductCartParamsDataProvider(): array
+    protected static function invalidProductCartParamsDataProvider(): array
     {
         return [
             // Simulate empty string for input field `productCartParams` and expect `GraphQlInputException`
@@ -98,36 +128,11 @@ class ExpressInitResolverTest extends AbstractAdyenTestCase
     }
 
     /**
-     * See the data provider for test case explanations.
+     * Data provider for abstract test case testSuccessfulResolving()
      *
-     * @dataProvider invalidProductCartParamsDataProvider
-     *
-     * @return void
-     * @throws GraphQlInputException|LocalizedException
+     * @return array
      */
-    public function testProductCartParamsShouldThrowException($productCartParams)
-    {
-        $this->expectException(GraphQlInputException::class);
-
-        $args = [
-            'productCartParams' => $productCartParams
-        ];
-
-        $this->expressInitResolver->resolve(
-            $this->fieldMock,
-            $this->contextMock,
-            $this->infoMock,
-            [],
-            $args
-        );
-    }
-
-    /**
-     * Data provider for case testSuccessfulResolving
-     *
-     * @return array[]
-     */
-    public static function validInputArgumentsForSuccessTest(): array
+    public static function successfulResolverDataProvider(): array
     {
         return [
             [
@@ -160,82 +165,38 @@ class ExpressInitResolverTest extends AbstractAdyenTestCase
     }
 
     /**
-     * Assets successful generation of the Value class after resolving
-     * the mutation with different set of inputs.
+     * Data provider for abstract test case testResolverShouldThrowExceptionWithEmptyArgument()
      *
-     * @dataProvider validInputArgumentsForSuccessTest
-     *
-     * @return void
-     * @throws GraphQlInputException|LocalizedException
+     * @return array[]
      */
-    public function testSuccessfulResolving($args)
+    protected static function emptyArgumentAssertionDataProvider(): array
     {
-        $productCartParamsMock = $this->createMock(ProductCartParams::class);
-        $this->cartParamsInterfaceFactoryMock->method('create')->willReturn($productCartParamsMock);
-
-        $returnValueMock = $this->createMock(Value::class);
-        $this->valueFactoryMock->method('create')->willReturn($returnValueMock);
-
-        $quoteIdMaskMock = $this->createGeneratedMock(QuoteIdMask::class, [
-            'load',
-            'getQuoteId'
-        ]);
-        $quoteIdMaskMock->method('load')->willReturn($quoteIdMaskMock);
-        $quoteIdMaskMock->method('getQuoteId')->willReturn(1);
-
-        $this->quoteIdMaskFactoryMock->method('create')
-            ->willReturn($quoteIdMaskMock);
-
-        $result = $this->expressInitResolver->resolve(
-            $this->fieldMock,
-            $this->contextMock,
-            $this->infoMock,
-            [],
-            $args
-        );
-
-        $this->assertInstanceOf(Value::class, $result);
+        return [
+            [
+                'args' => []
+            ],
+            [
+                'args' => [
+                    'productCartParams' => ''
+                ]
+            ]
+        ];
     }
 
     /**
-     * Asserts exception if the quote entity not found
+     * Data provider for abstract test case testMissingQuoteShouldThrowException()
      *
-     * @return void
-     * @throws GraphQlInputException
-     * @throws LocalizedException
+     * @return array
      */
-    public function testMissingQuoteShouldThrowException()
+    protected static function missingQuoteAssertionDataProvider(): array
     {
-        $this->expectException(LocalizedException::class);
-
-        $args = [
-            'productCartParams' => self::SUCCESSFUL_CART_PARAMS
+        return [
+            [
+                'args' => [
+                    'productCartParams' => self::SUCCESSFUL_CART_PARAMS,
+                    'adyenCartId' => 'Mock_adyenCartId'
+                ]
+            ]
         ];
-
-        $productCartParamsMock = $this->createMock(ProductCartParams::class);
-        $this->cartParamsInterfaceFactoryMock->method('create')->willReturn($productCartParamsMock);
-
-        $this->valueFactoryMock->method('create')
-            ->willThrowException(new ExpressInitException(__('Localized test exception!')));
-
-        $quoteIdMaskMock = $this->createGeneratedMock(QuoteIdMask::class, [
-            'load',
-            'getQuoteId'
-        ]);
-        $quoteIdMaskMock->method('load')->willReturn($quoteIdMaskMock);
-        $quoteIdMaskMock->method('getQuoteId')->willReturn(1);
-
-        $this->quoteIdMaskFactoryMock->method('create')
-            ->willReturn($quoteIdMaskMock);
-
-        $this->expressInitResolver->resolve(
-            $this->fieldMock,
-            $this->contextMock,
-            $this->infoMock,
-            [],
-            $args
-        );
-
-        $this->loggerMock->expects($this->once())->method('error');
     }
 }
