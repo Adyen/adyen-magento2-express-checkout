4eab702d88a5464c7cfd1781fc9cc1fee4540304
Khushboo <khushboo.singhvi@adyen.com>
2025-06-24 10:24:50 +0200

    [ECP-9669] Fixing frictionless 3DS2 empty modal issue (#193)
    * [ECP-9669] Fixing frictionless 3DS2 empty modal issue
    
    * [ECP-9669] fining typos
    
    * [ECP-9669] Checking the failing pipeline
    
    * [ECP-9669] Checking the failing pipeline
    
    * [ECP-9669] Checking the failing pipeline
    
    * [ECP-9669] Checking the failing pipeline
    
    * [ECP-9669] Checking the failing pipeline
    
    * [ECP-9669] Checking the failing pipeline
    
    * [ECP-9669] Checking the failing pipeline
    
    * [ECP-9669] Checking the failing pipeline
    
    * Update .github/Makefile
    
    Co-authored-by: Can Demiralp <can.demiralp@adyen.com>
    
    ---------
    
    Co-authored-by: Can Demiralp <can.demiralp@adyen.com>

--- view/frontend/web/js/googlepay/button.js
+++ view/frontend/web/js/googlepay/button.js
@@ -517,7 +517,6 @@ define([
 
                 adyenPaymentService.paymentDetails(request, self.orderId).
                 done(function(responseJSON) {
-                    fullScreenLoader.stopLoader();
                     self.handleAdyenResult(responseJSON, self.orderId);
                 }).
                 fail(function(response) {
@@ -545,14 +544,19 @@ define([
                 var self = this;
                 let popupModal;
 
-                fullScreenLoader.stopLoader();
-
                 if (action.type === 'threeDS2' || action.type === 'await') {
                     popupModal = self.showModal();
                 }
 
                 try {
-                    self.checkoutComponent.createFromAction(action).mount('#' + this.modalLabel);
+                    self.checkoutComponent.createFromAction(action, {
+                        onActionHandled: function (event) {
+                            if (event.componentType === "3DS2Challenge") {
+                                fullScreenLoader.stopLoader();
+                                popupModal.modal('openModal');
+                            }
+                        }
+                    }).mount('#' + this.modalLabel);
                 } catch (e) {
                     console.log(e);
                     self.closeModal(popupModal);
@@ -566,7 +570,8 @@ define([
                     this.messageContainer,
                     this.orderId,
                     this.modalLabel,
-                    this.isPlaceOrderActionAllowed
+                    this.isPlaceOrderActionAllowed,
+                    false
                 );
 
                 $("." + this.modalLabel + " .action-close").hide();
