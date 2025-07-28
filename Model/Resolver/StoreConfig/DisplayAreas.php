a2ec515351869c39d119cb351c98783dcfc18ad1
Dimitri BOUTEILLE <dimitri.bouteille@reflet-digital.com>
2025-07-28 11:34:36 +0200

    💎 Add display areas graphQl configuration

---
+++ Model/Resolver/StoreConfig/DisplayAreas.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2024 Adyen N.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <magento@adyen.com>
+ */
+declare(strict_types=1);
+
+namespace Adyen\ExpressCheckout\Model\Resolver\StoreConfig;
+
+use Adyen\ExpressCheckout\Model\Config\Source\ShortcutAreas;
+use Adyen\ExpressCheckout\Model\Configuration;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+
+class DisplayAreas implements ResolverInterface
+{
+    protected const AREA_MAPPING = [
+        ShortcutAreas::CART_PAGE_VALUE => 'CART_PAGE',
+        ShortcutAreas::MINICART_VALUE => 'MINI_CART',
+        ShortcutAreas::PRODUCT_VIEW_VALUE => 'PRODUCT_PAGE',
+    ];
+
+    /**
+     * DisplayAreas Constructor
+     *
+     * @param Configuration $configuration
+     */
+    public function __construct(
+        public Configuration $configuration,
+    ) {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null): array
+    {
+        $areas = $this->configuration->getShowPaymentMethodOn($this->getPaymentMethodVariant($field));
+        if ($areas === []) {
+            return [];
+        }
+
+        return array_filter(array_map(function (mixed $area) {
+            if (!is_numeric($area)) {
+                return null;
+            }
+
+            return self::AREA_MAPPING[$area] ?? null;
+        }, $areas));
+    }
+
+    /**
+     * @param Field $field
+     * @return string
+     * @throws LocalizedException
+     */
+    protected function getPaymentMethodVariant(Field $field): string
+    {
+        /**
+         * Extract a method variant from the field name
+         * adyen_express_paypal_express_display_areas => paypal_express
+         */
+        preg_match('/^adyen_express_([a-zA-Z_-]+)_display_areas$/', $field->getName(), $matches);
+        $variant = $matches[1] ?? null;
+        if ($variant === null || $variant === '') {
+            throw new LocalizedException(__("Invalid variant."));
+        }
+
+        return $variant;
+    }
+}
