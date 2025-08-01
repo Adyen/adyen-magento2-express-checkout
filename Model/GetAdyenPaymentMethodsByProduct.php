6e18a7dffc41f7d698c0276801bac9c73bd1024a
khushboos <khushboo.singhvi@adyen.com>
2025-08-01 15:08:45 +0200

    [ECP-9759] Supporting PhpStan on develop-3

--- Model/GetAdyenPaymentMethodsByProduct.php
+++ Model/GetAdyenPaymentMethodsByProduct.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 namespace Adyen\ExpressCheckout\Model;
 
+use Adyen\Model\Checkout\PaymentMethodsRequest;
 use Adyen\Payment\Helper\ChargedCurrency;
 use Adyen\Payment\Helper\Config;
 use Adyen\Payment\Helper\Data;
+use Adyen\Payment\Helper\Locale;
 use Adyen\Payment\Model\AdyenAmountCurrency;
 use Adyen\Payment\Model\AdyenAmountCurrencyFactory;
 use Magento\Catalog\Api\Data\ProductInterface;
@@ -28,6 +30,11 @@ class GetAdyenPaymentMethodsByProduct implements GetAdyenPaymentMethodsByProduct
     private $adyenHelper;
 
     /**
+     * @var Data
+     */
+    private $localeHelper;
+
+    /**
      * @var Config
      */
     private $adyenConfigHelper;
@@ -45,6 +52,7 @@ class GetAdyenPaymentMethodsByProduct implements GetAdyenPaymentMethodsByProduct
     /**
      * @param AdyenAmountCurrencyFactory $adyenAmountCurrencyFactory
      * @param Data $adyenHelper
+     * @param Locale $localeHelper
      * @param Config $adyenConfigHelper
      * @param ChargedCurrency $chargedCurrency
      * @param ScopeConfigInterface $scopeConfig
@@ -52,12 +60,14 @@ class GetAdyenPaymentMethodsByProduct implements GetAdyenPaymentMethodsByProduct
     public function __construct(
         AdyenAmountCurrencyFactory $adyenAmountCurrencyFactory,
         Data $adyenHelper,
+        Locale $localeHelper,
         Config $adyenConfigHelper,
         ChargedCurrency $chargedCurrency,
         ScopeConfigInterface $scopeConfig
     ) {
         $this->adyenAmountCurrencyFactory = $adyenAmountCurrencyFactory;
         $this->adyenHelper = $adyenHelper;
+        $this->localeHelper = $localeHelper;
         $this->adyenConfigHelper = $adyenConfigHelper;
         $this->chargedCurrency = $chargedCurrency;
         $this->scopeConfig = $scopeConfig;
@@ -85,13 +95,15 @@ class GetAdyenPaymentMethodsByProduct implements GetAdyenPaymentMethodsByProduct
         if (!$merchantAccount) {
             return [];
         }
+        /** @var AdyenAmountCurrency $quoteAmountCurrency */
+        $quoteAmountCurrency = $this->chargedCurrency->getQuoteAmountCurrency($quote);
         $configuredChargeCurrency = $this->adyenConfigHelper->getChargedCurrency(
             $quote->getStoreId()
         );
         $currencyCode = $configuredChargeCurrency === ChargedCurrency::BASE ?
             $quote->getBaseCurrencyCode() :
             $quote->getCurrency()->getQuoteCurrencyCode();
-
+        /** @var AdyenAmountCurrency $adyenAmountCurrency */
         $adyenAmountCurrency = $this->adyenAmountCurrencyFactory->create([
             'amount' => $product->getFinalPrice(),
             'currencyCode' => $currencyCode
@@ -100,7 +112,7 @@ class GetAdyenPaymentMethodsByProduct implements GetAdyenPaymentMethodsByProduct
             "channel" => "Web",
             "merchantAccount" => $merchantAccount,
             "countryCode" => $this->getCurrentCountryCode($store),
-            "shopperLocale" => $this->adyenHelper->getCurrentLocaleCode($store->getId()),
+            "shopperLocale" => $this->localeHelper->getCurrentLocaleCode($store->getId()),
             "amount" => [
                 "value" => $this->adyenHelper->formatAmount($adyenAmountCurrency->getAmount(), $currencyCode),
                 "currency" => $currencyCode
@@ -109,10 +121,9 @@ class GetAdyenPaymentMethodsByProduct implements GetAdyenPaymentMethodsByProduct
         $adyenClient = $this->adyenHelper->initializeAdyenClient(
             $store->getId()
         );
-        $service = $this->adyenHelper->createAdyenCheckoutService($adyenClient);
+        $service = $this->adyenHelper->initializePaymentsApi($adyenClient);
         $response = $service->paymentMethods(
-            $paymentMethodRequest,
-            $store
+            new PaymentMethodsRequest($paymentMethodRequest)
         );
         if (!$response) {
             return [];
